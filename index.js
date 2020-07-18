1d5bd746adfd6c8230613c45a34bc25a8902699c
Susul-1312 <sulpizh@gmail.com>
2020-07-18 15:47:10 +0200

    added public serving

--- index.js
+++ index.js
@@ -3,22 +3,14 @@ const bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 4200;
 
-// always keep on top
+// logging requests, always keep on top
 app.use(function (req, res, next) {
   console.log("Request recieved at " + Date.now() + " from " + req.ip);
   next();
 });
 app.use(bodyParser.json());
 
-app.use("/", function (req, res, next) {
-  res.send("Index");
-  console.log("send Index");
-  next();
-});
-
-app.use(function (req, res) {
-  // end of request
-});
+app.use("/", express.static("public"));
 
 app.listen(port, function () {
   console.log("App listening on Port: " + port);
