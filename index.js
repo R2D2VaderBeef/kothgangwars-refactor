6660d4894dda333722383c3eff633548fa5b8a5f
Susul-1312 <sulpizh@gmail.com>
2020-07-18 18:28:30 +0200

    Public files now served even tho vader hasnt done his job yet

--- index.js
+++ index.js
@@ -22,9 +22,7 @@ app.use(function (req, res, next) {
 });
 app.use(bodyParser.json());
 
-app.use("/", function (req, res) {
-  res.send("index");
-});
+app.use(express.static("public"));
 
 app.listen(port, function () {
   console.log("App listening on Port: " + port);
