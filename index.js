a973897d181c82bf86dd70803fd3e49e9221cc82
Susul-1312 <sulpizh@gmail.com>
2020-07-18 15:10:52 +0200

    commit

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 4200;
 // always keep on top
-app.use(function (req, res, next) {
-    console.log('Request recieved at ' + Date.now() + ' from ' + req.ip);
-    next();
+app.use(function(req, res, next){
+  console.log("Request recieved at " + Date.now() + " from " + req.ip);
+  next();
 });
-app.use('/', function (req, res, next) {
-    res.send('Index');
-    console.log('send Index');
-    next();
-});
-
-app.use(function (req, res) {
-    // end of request
+app.use("/", function(req, res, next) {
+  res.send("Index");
+  console.log("send Index");
+  next()
 });
 
-app.listen(port, function () {
-    console.log('App listening on Port: ' + port);
+app.use(function(req, res){
+  // end of request
+});
+
+app.listen(port, function(){
+  console.log("App listening on Port: " + port)
 });
