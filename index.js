1486c22c31f38e8f75f67ace12ccc7355dc89c58
Susul-1312 <sulpizh@gmail.com>
2020-07-18 14:10:53 +0200

    added some express stuff

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 const express = require('express');
+const app = express();
+
+// always keep on top
+app.use(function(req, res, next){
+  console.log("Request recieved at " + Date.now() + " From " + req.ip);
+  next();
+});
+app.use("/", function(req, res, next) {
+  res.send("Index");
+  next();
+})
+
+
+app.use(function(req, res){
+  console.log("request ended");
+})
+console.log(process.env.PORT);
+app.listen(process.env.PORT)
